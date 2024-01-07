a0fad0519e5e65d0e35578e97459e685647c1096
satrioed <32364936+satrio13@users.noreply.github.com>
2024-01-07 11:35:29 +0700

    Update Tahun.php

--- application/modules/tahun/controllers/Tahun.php
+++ application/modules/tahun/controllers/Tahun.php
@@ -24,7 +24,7 @@ class Tahun extends CI_Controller
 	{
 		$page = $_POST['start'] / $_POST['length'] + 1;
 		$limit = $_POST['length'];
-		$search = $_POST['search']['value'];
+		$search = urlencode($_POST['search']['value']);
 		if(!empty($search))
 		{
 			$url = api_url()."list-tahun?page=$page&limit=$limit&search=$search";
