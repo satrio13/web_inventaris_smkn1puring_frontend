2f783cc53ebb4749b66bfcb5dedbacc03777e63c
satrioed <32364936+satrio13@users.noreply.github.com>
2024-01-07 11:33:49 +0700

    Update Pengambilan.php

--- application/modules/pengambilan/controllers/Pengambilan.php
+++ application/modules/pengambilan/controllers/Pengambilan.php
@@ -24,7 +24,7 @@ class Pengambilan extends CI_Controller
 	  {
       $page = $_POST['start'] / $_POST['length'] + 1;
       $limit = $_POST['length'];
-      $search = $_POST['search']['value'];
+      $search = urlencode($_POST['search']['value']);
       if(!empty($search))
       {
         $url = api_url()."list-baranghp?page=$page&limit=$limit&search=$search";
@@ -74,7 +74,7 @@ class Pengambilan extends CI_Controller
 	  {
       $page = $_POST['start'] / $_POST['length'] + 1;
       $limit = $_POST['length'];
-      $search = $_POST['search']['value'];
+      $search = urlencode($_POST['search']['value']);
       if(!empty($search))
       {
         $url = api_url()."list-pengambilan?page=$page&limit=$limit&search=$search";
