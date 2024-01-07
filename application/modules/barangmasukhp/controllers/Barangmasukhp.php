8b15efe6bf84fa47be2ae1840cd1d5d82217cd96
satrioed <32364936+satrio13@users.noreply.github.com>
2024-01-07 11:31:39 +0700

    Update Barangmasukhp.php

--- application/modules/barangmasukhp/controllers/Barangmasukhp.php
+++ application/modules/barangmasukhp/controllers/Barangmasukhp.php
@@ -29,7 +29,7 @@ class Barangmasukhp extends CI_Controller
 	{
 		$page = $_POST['start'] / $_POST['length'] + 1;
 		$limit = $_POST['length'];
-		$search = $_POST['search']['value'];
+		$search = urlencode($_POST['search']['value']);
 		if(!empty($search))
 		{
 			$url = api_url()."list-barangmasukhp?page=$page&limit=$limit&search=$search";
